5f509df8ab19714d78371fb9a7c550b0690e5a35
Dan_Imai <oinarhythm0508@gmail.com>
2016-02-02 15:26:07 +0900

    Initial commit

---
+++ app/src/main/java/com/bizan/mobile10/swipeablelist/ItemData.java
@@ -0,0 +1,13 @@
+package com.bizan.mobile10.swipeablelist;
+
+public class ItemData {
+    private String mText;
+
+    public ItemData(String text) {
+        this.mText = text;
+    }
+
+    public String getText() {
+        return mText;
+    }
+}
